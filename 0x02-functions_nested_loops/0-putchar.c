e14dbf80d7d0b7c791052e5e699b41d329fd3e99
abbaaji2004@yahoo.com <Mohammed Abba Aji>
2022-12-13 17:59:34 -0800

    Added 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main -Entry point
@@ -18,6 +18,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 
 }
