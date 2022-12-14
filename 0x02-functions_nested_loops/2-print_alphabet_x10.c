e42311ac94b2267b75073ed9cac1b10d777c8eee
MxRepo <46465455+MxRepo@users.noreply.github.com>
2022-12-13 19:40:03 -0800

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Check holberton
+ * print_alphabetx10 - Check holberton
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: Nothing.
