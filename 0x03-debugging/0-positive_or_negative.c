076ef1dc868bf9803a320facde0e8085f5f04228
MxRepo <46465455+MxRepo@users.noreply.github.com>
2022-12-15 16:57:14 -0800

    Added 0-positive_or_negative

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = i;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
+}
