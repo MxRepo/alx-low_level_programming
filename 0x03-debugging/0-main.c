fc97a42fe4d4e8738f1be37d11fc4218239a0270
abbaaji2004@yahoo.com <Mohammed Abba Aji>
2022-12-15 16:49:28 -0800

    Added 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <time.h>
 
-
-void positive_or_negative(int i)
-
-{
-	int n;
-
-	srand(time(0));
-	n = i;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
-	return (0);
-}
 
 /**
  * main - Test function for positive or negative
